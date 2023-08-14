ee2dc0c97107dd1b7483dc5143aa48929a90ab19
junkfreaknet <junk.freak.net@gmail.com>
2023-08-14 16:55:15 +0900

    2023 08 14mon16:54 sion db and pug

--- controllers/detailsController.js
+++ controllers/detailsController.js
@@ -4,18 +4,47 @@ const {body,validationResult}=require('express-validator');
 var async=require('async');
 var asyncHandler=require('express-async-handler');
 
-const mysql=require('mysql2');
-
+var result  //result reciever query
+var allDetails
 //display all details
 exports.details_list=asyncHandler(async(req,res,next)=>{
     //const allDetails="*****";
-    allDetails=[
-        "1","2","3","4","6"
-    ];
+    /***allDetails=[
+        {FirstName:"aaaaa",LastName:"bbbbb",FullName:"aaaaabbbbb"},
+        {FirstName:"ccccc",LastName:"ddddd",FullName:"cccccddddd"}
+    ];***/
     //console.log('abcdefg');
     //console.log(allDetails);
     //res.render('details',{details_list: allDetails});
-    res.render('details',{details: ["1","2","3"]});
+    
+    const mysql=require('mysql2');
+    const cnnctn= await mysql.createConnection({
+        host:'public.nhumf.tyo2.database-hosting.conoha.io',
+        user:'nhumf_sion',
+        password:'Noriyuki6403',
+        database:'nhumf_sion'
+    })
+    try{
+                
+        //const [rows,fields]=await cnnctn.execute('select distinct syu_ymd from select_locale_child_202212011001_cp_csv');
+        //console.log(rows[0]);
+        //console.log(rows[0].syu_ymd);
+        result=await cnnctn.execute('select distinct syu_ymd from select_locale_child_202212011001_cp_csv');
+        console.log(rows[0].syu_ymd);
+        allDetails=result;
+    }
+    catch(e){
+        console.log('*****');
+        console.log("*****error occured:"+e);
+        console.log('*****');
+
+    }
+    finally{
+        cnnctn.end();
+        console.log('*****finally connection closed.');
+    }
+
+    //at last exports result
+    res.render('details',{title:'display details.',details:allDetails});
 })
 
-//export
--- routes/details.js
+++ routes/details.js
@@ -5,10 +5,11 @@ var detailsController=require('../controllers/detailsController');
 //var de=require('../controllers/detailsController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
+/**router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
   res.render('details',{title:'SION details',details:detailsController.details_list});
-});
+});**/
+router.get('/',detailsController.details_list);
 
 router.get('/cool',function(req,res,next){
   res.send('you are so cool.');
